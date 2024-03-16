f64407033f4e7b13b5d031f5aaba042599c3eda0
Md Shahid Alam <md.shahidalam.cse24@heritageit.edu.in>
2024-03-16 18:05:05 +0530

    included layouts and navbar and footer and styled homepage and form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const Listing = require("./models/listings");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 const port = 8080;
@@ -12,6 +13,9 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(port, () => {
   console.log("App is listening on port", port);
